18cfe0d7aec9206a6fc0a558d182276056197d52
AlexisLoya <alexloy117@gmail.com>
2021-02-20 16:11:12 -0600

    Screen navigation

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,19 +1,47 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react'
+import { View, Text, Pressable, StyleSheet} from 'react-native'
 
 
 class CoinsScreen extends Component {
+
+    handlePress = () =>{
+        console.log("Go to details", this.props)
+        //Move to other Screen navigate('name in the Stack')
+        this.props.navigation.navigate('Detail')
+    }
+
     render() {
         return (
-            <View>
-                <Text>Coins Screen</Text>
-                <Text>Coins Screen</Text>
-                <Text>Coins Screen</Text>
-                <Text>Coins Screen</Text>
-
+            <View style={styles.container}>
+                <Text style={styles.titleText}>Coins Screen</Text>
+                <Pressable onPress={this.handlePress} style={styles.btn}>
+                    <Text style={styles.btnText}>Go to Detail</Text>
+                    </Pressable>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: "grey"
+    },
+    titleText:{
+        color: "#fff",
+        textAlign:"center",
+        fontSize: 30
+    },
+    btn: {
+        padding:8,
+        backgroundColor:"green",
+        borderRadius: 8,
+        margin: 16
+    },
+    btnText:{
+       color: "#fff",
+       textAlign: "center"
+    }
+});
 
 export default CoinsScreen;
--- src/components/coins/CoinsStack.js
+++ src/components/coins/CoinsStack.js
@@ -1,24 +1,31 @@
-import React from 'react'; // simpre 
-import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react' // simpre
+import { createStackNavigator } from '@react-navigation/stack'
 
-import CoinsScreen from './CoinsScreen'; //import Screen
+import CoinsScreen from './CoinsScreen'; //import CoinsScreen
+import CoinDetailScreen from './CoinDetailsScreen'; //import CoinDetails
+
 
 const Stack = createStackNavigator();
 
-const CoinsStack = () =>{
+const CoinsStack = () => {
 
     return (
         //Contenedor padre
         <Stack.Navigator>
 
-<Stack.Screen
-        name="Coins"
-        component={CoinsScreen}
-      />
+            <Stack.Screen
+             name="Coins"
+             component={CoinsScreen}
+            />
+
+            <Stack.Screen
+            name="Detail"
+            component={CoinDetailScreen}
+            />
 
         </Stack.Navigator>
     );
 }
 
 
-export default CoinsStack
+export default CoinsStack;
