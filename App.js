aecaa40eea996036952e780829eb03bcc98f1e6b
AlexisLoya <alexloy117@gmail.com>
2021-02-21 23:51:29 -0600

    Implement a bottom navigator

--- App.js
+++ App.js
@@ -1,17 +1,46 @@
 
 import React from 'react'
+import { Image, StyleSheet } from 'react-native'
 import { NavigationContainer }from '@react-navigation/native'
 import CoinsStack from './src/components/coins/CoinsStack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Colors from 'cryptoTracker/src/res/Colors';
 
+const Tabs = createBottomTabNavigator();
 
 const App = () => {
   return (
     //Contenedor 
     <NavigationContainer>
-      <CoinsStack />
+      <Tabs.Navigator 
+        tabBarOptions={{
+          tintColor: "#fefefe",
+          style:{
+            borderTopWidth: 0,
+            backgroundColor: Colors.blackPearl
+          }
+        }}
+      >
+        <Tabs.Screen
+          name="Coins"
+          component={CoinsStack}
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <Image
+                style={{ tintColor: color, width: size, height: size }}
+                source={require('cryptoTracker/src/assets/bank.png')}
+              />
+            )
+          }}
+        />
+      </Tabs.Navigator>
     </NavigationContainer>
   );
 };
 
+const styles = StyleSheet.create({
+  bankIcon:{
 
+  }
+});
 export default App;
